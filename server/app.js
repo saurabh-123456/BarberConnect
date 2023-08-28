b06cdc8bc405125b753c028321e52c584e1eb6fc
Gyaneshwar <gyaneshwar.knp.2019@gmail.com>
2023-08-28 22:35:52 +0530

    mongo_connect

--- server/app.js
+++ server/app.js
@@ -1,16 +1,30 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
 const _ = require("dotenv");
-require('dotenv').config(); 
+require("dotenv").config();
 
 var cors = require("cors");
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 app.use(express.json());
 app.use(cors());
 
+const URL = process.env.mongo_URL;
+console.log(URL);
+
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongo succesfully");
+  })
+  .catch((e) => console.log(e));
+
 // app.use("/api/auth", require("./routes/auth"));
 
 app.listen(port, () => {
-  console.log(`BarberConnect is listening on port ${port}`)
-})
+  console.log(`BarberConnect is listening on port ${port}`);
+});
